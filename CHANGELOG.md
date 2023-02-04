46f4de4d4da146abb7058383c998539ab85bd125
larry <xspanni@gmail.com>
2023-02-04 16:51:36 +0800

    为发布到pub.dev做修改，增加license、修改版本号等

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.1
+## 0.1.0
 
-* TODO: Describe initial release.
+* 实现加载simple全文索引插件
--- test/quantum_test.dart
+++ test/quantum_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:quantum/quantum.dart';
+import 'package:quantum_sqlite/quantum_sqlite.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('quantum');
